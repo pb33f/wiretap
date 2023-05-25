941a786d185cadb4391156719ff367fd70302ed3
Dave Shanley <dave@quobix.com>
2023-05-25 06:57:28 -0400

    fixed build after munging it.
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import copy from 'rollup-plugin-copy';
-import * as path from "path"
 import typescript from "@rollup/plugin-typescript";
+import * as path from "path"
 
 
 const mode = process.env.NODE_ENV || "production"
@@ -16,26 +16,12 @@ const vitePath = `${paths[mode]}`
 export default defineConfig({
     plugins: [tsconfigPaths()],
     build: {
-        manifest: true,
-        minify: true,
-        reportCompressedSize: true,
-        lib: {
-            entry: path.resolve(__dirname, "src/index.ts"),
-            fileName: "index",
-            formats: ["es", "cjs"],
-        },
         outDir: './dist',
         rollupOptions: {
             external: [
                 /^node:.*/,
             ],
-            plugins: [
-                typescript({
-                    sourceMap: false,
-                    declaration: true,
-                    outDir: "dist",
-                }),
-                copy({
+            plugins: [copy({
                     targets: [
                         {
                             src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
@@ -43,8 +29,14 @@ export default defineConfig({
                         }
                     ],
                 hook: 'writeBundle',
-                })
+                }),
+                typescript({
+                    sourceMap: false,
+                    declaration: true,
+                    outDir: "dist",
+                }),
             ],
+
         }
     }
 })
