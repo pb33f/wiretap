8e18f4451555ed4cb315a3faa83b5f389737e322
Dave Shanley <dave@quobix.com>
2023-07-02 10:27:36 -0400

    Path configuration is coming together.
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- config/paths_test.go
+++ config/paths_test.go
@@ -39,13 +39,45 @@ paths:
 	// compile paths
 	wcConfig.CompilePaths()
 
-	res := FindPath("/pb33f/test/123", wcConfig)
+	res := FindPaths("/pb33f/test/123", wcConfig)
 	assert.Len(t, res, 1)
 
-	res = FindPath("/pb33f/test/123/sing/song", wcConfig)
+	res = FindPaths("/pb33f/test/123/sing/song", wcConfig)
 	assert.Len(t, res, 1)
 
-	res = FindPath("/pb33f/no-match/wrong", wcConfig)
+	res = FindPaths("/pb33f/no-match/wrong", wcConfig)
 	assert.Len(t, res, 0)
+
+}
+
+func TestRewritePath(t *testing.T) {
+
+	config := `
+paths:
+  /pb33f/test/**:
+    target: http://localhost:9093/
+    secure: false
+    pathRewrite:
+      '^/pb33f/test/': ''`
+
+	viper.SetConfigType("yaml")
+	verr := viper.ReadConfig(strings.NewReader(config))
+	assert.NoError(t, verr)
+
+	paths := viper.Get("paths")
+	var pc map[string]*shared.WiretapPathConfig
+
+	derr := mapstructure.Decode(paths, &pc)
+	assert.NoError(t, derr)
+
+	wcConfig := &shared.WiretapConfiguration{
+		PathConfigurations: pc,
+	}
+
+	// compile paths
+	wcConfig.CompilePaths()
+
+	path := RewritePath("/pb33f/test/123/slap/a/chap", wcConfig)
+	assert.Equal(t, "http://localhost:9093/123/slap/a/chap", path)
 
 }
