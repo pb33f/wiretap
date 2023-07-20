f02aca2256dc5c8b2ef26daa46b3bbee5f50c82c
Dave Shanley <dave@quobix.com>
2023-07-20 07:40:14 -0400

    More tuning of schema violation UI
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- ui/src/components/violation/violation.css.ts
+++ ui/src/components/violation/violation.css.ts
@@ -10,39 +10,40 @@ export default css`
   p.reason {
     margin-top: 0;
   }
-  
+
   .violation::part(base) {
     background: none;
     border: dashed 1px var(--secondary-color-dimmer);
   }
-  
+
 
   h3 {
     margin-top: 20px;
     margin-bottom: 10px;
     font-family: var(--font-stack-heading);
   }
-  
+
   .violation-meta {
     display: flex;
     justify-content: space-between;
     color: var(--darker-font-color)
   }
-  
+
   .location-meta {
     font-size: 0.9em;
     padding-top: 5px;
   }
-  
+
   .jump-spec {
     color: var(--primary-color);
   }
+
   .jump-spec:hover {
     text-decoration: underline;
     cursor: pointer;
   }
-  
-  
+
+
   .validation-type::part(base) {
     background: none;
     border: dashed 1px var(--secondary-color-dimmer);
@@ -54,7 +55,7 @@ export default css`
     border: dashed 1px var(--secondary-color-dimmer);
     color: var(--secondary-color)
   }
-  
+
   .schema-violation-objects {
     display: flex;
     margin-top: 5px;
@@ -72,20 +73,21 @@ export default css`
   }
 
   .schema-violation-object pre {
-    white-space: pre-wrap;      
+    white-space: pre-wrap;
   }
+
   .schema-violation-object pre > code {
     white-space: pre-wrap;
   }
-  
+
   .line-num {
     color: var(--dark-font-color);
   }
-  
+
   .line-active {
     color: red;
   }
-  
+
   .schema-violation-objects .schema-violation-object h4 {
     font-size: 0.8rem;
     color: var(--primary-color);
@@ -95,7 +97,7 @@ export default css`
     margin-top: 0;
     margin-bottom: 0;
   }
-  
+
 
   .schema-violation-objects .schema-violation-object::-webkit-scrollbar {
     width: 8px;
@@ -109,11 +111,17 @@ export default css`
     box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     background: var(--secondary-color-lowalpha);
   }
-  
-  
+
+
+  .schema-radio-button::part(base) {
+    --sl-color-primary-600: var(--secondary-color);
+  }
+
+  .schema-radio-button::part(button) {
+    font-family: var(--mono-font-stack);
+  }
+
   .schema-violations {
-    //max-height: 300px;
-    //overflow-y: auto;
   }
 
   .schema-violations::-webkit-scrollbar {
@@ -129,8 +137,18 @@ export default css`
     background: var(--secondary-color-lowalpha);
   }
 
+  .schema-data-switch-input {
+    width: 50%;
+    padding-top: 3px;
+  }
+
+  .schema-type-select {
+    text-align: right;
+    width: 50%
+  }
+
   .schema-radio-group {
-    float: right
+
   }
 
   a.schema-violation-link {
@@ -138,16 +156,21 @@ export default css`
     font-family: var(--mono-font-stack);
     font-size: 0.8rem;
   }
+
   a.schema-violation-link:visited {
     color: var(--primary-color);
   }
 
   .how-to-fix {
-    
+
   }
-  
+
   .schema-data-switch {
+    display: flex;
     text-align: left;
+    margin-bottom: 10px;
+    height: 30px;
+    font-family: var(--mono-font-stack);
   }
-  
+
 `
