8b7283970a61b094eac7d65451a3cd2c663234f6
quobix <dave@quobix.com>
2024-01-13 09:09:36 -0500

    tuned up styles
    Signed-off-by: quobix <dave@quobix.com>

--- ui/src/components/controls/filters.css.ts
+++ ui/src/components/controls/filters.css.ts
@@ -3,77 +3,86 @@ import {css} from "lit";
 
 export default css`
 
-  .label-on-left {
-    --label-width: 5rem;
-    --gap-width: 1rem;
-  }
+    .label-on-left {
+        --label-width: 5rem;
+        --gap-width: 1rem;
+    }
 
-  .label-on-left + .label-on-left {
-    margin-top: var(--sl-spacing-medium);
-  }
+    .label-on-left + .label-on-left {
+        margin-top: var(--sl-spacing-medium);
+    }
 
-  .label-on-left::part(form-control) {
-    display: grid;
-    grid: auto / var(--label-width) 1fr;
-    gap: var(--sl-spacing-3x-small) var(--gap-width);
-    align-items: center;
-    font-family: var(--font-stack);
-  }
+    .label-on-left::part(form-control) {
+        display: grid;
+        grid: auto / var(--label-width) 1fr;
+        gap: var(--sl-spacing-3x-small) var(--gap-width);
+        align-items: center;
+        font-family: var(--font-stack);
+    }
 
-  .label-on-left::part(form-control-label) {
-    text-align: right;
-  }
+    .label-on-left::part(form-control-label) {
+        text-align: right;
+    }
 
-  .label-on-left::part(form-control-help-text) {
-    grid-column-start: 2;
-  }
+    .label-on-left::part(form-control-help-text) {
+        grid-column-start: 2;
+    }
 
-  hr {
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-  
-  .keywords {
-    border: 1px dashed var(--secondary-color-dimmer);
-    padding: 5px;
-    min-height: 40px;
-    margin-top: 20px;
-  }
+    hr {
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
 
-  .chains {
-    border: 1px dashed var(--primary-color-lowalpha);
-    padding: 5px;
-    min-height: 40px;
-    margin-top: 20px;
-  }
-  
-  .keyword {
-    transition: var(--sl-transition-fast) opacity;
-  }
-  
-  .keyword::part(base) {
-    font-family: var(--font-stack);
-    border: 1px dashed var(--secondary-color);
-    margin-bottom: 5px;
-  }
+    .keywords {
+        border: 1px dashed var(--secondary-color-dimmer);
+        padding: 5px;
+        min-height: 40px;
+        margin-top: 20px;
+    }
 
-  .chain {
-    transition: var(--sl-transition-fast) opacity;
-  }
+    .chains {
+        border: 1px dashed var(--primary-color-lowalpha);
+        padding: 5px;
+        min-height: 40px;
+        margin-top: 20px;
+    }
 
-  .chain::part(base) {
-    font-family: var(--font-stack);
-    border: 1px dashed var(--primary-color);
-    margin-bottom: 5px;
-  }
-  
-  p {
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-  
-  .chain-input::part(base) {
-    --sl-input-border-color: var(--primary-color);
-  }
-  
+    .keyword {
+        transition: var(--sl-transition-fast) opacity;
+    }
+
+    .keyword::part(base) {
+        font-family: var(--font-stack), monospace;
+        border: 1px dashed var(--secondary-color);
+        margin-bottom: 5px;
+        border-radius: 0;
+    }
+
+    .chain {
+        transition: var(--sl-transition-fast) opacity;
+    }
+
+    .chain::part(base) {
+        font-family: var(--font-stack), monospace;
+        border: 1px dashed var(--primary-color);
+        margin-bottom: 5px;
+        border-radius: 0;
+    }
+
+    p {
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+
+    .chain-input::part(base) {
+        --sl-input-border-color: var(--primary-color);
+    }
+    
+    sl-select::part(combobox) {
+        border-radius: 0;
+    }
+
+    sl-input::part(base) {
+        border-radius: 0;
+    }
 `
--- ui/src/components/controls/settings.css.ts
+++ ui/src/components/controls/settings.css.ts
@@ -4,19 +4,35 @@ import {css} from "lit";
 
 export default css`
 
-  sl-icon.report  {
-    font-size: 1.4rem;
-    display: inline-block;
-  }
-  
-  label {
-    display: block;
-    padding-bottom: 10px;
-  }
-  
-  hr {
-    margin-top: 30px;
-    margin-bottom: 30px;
-  }
-  
+    sl-icon.report {
+        font-size: 1.4rem;
+        display: inline-block;
+    }
+
+    label {
+        display: block;
+        padding-bottom: 10px;
+    }
+
+    hr {
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+
+    sl-select::part(combobox) {
+        border-radius: 0;
+    }
+
+    sl-input::part(base) {
+        border-radius: 0;
+    }
+
+    sl-button::part(base) {
+        font-family: var(--font-stack), monospace;
+        border: 1px dashed;
+        border-radius: 0;
+    }
+    
+    
+
 `
