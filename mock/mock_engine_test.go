fc63ca02433ff0645545fa8dfec8a4ceb726cd94
Jacob Moore <jacobm@splunk.com>
2024-03-07 16:14:09 -0500

    Fixed broken test

--- mock/mock_engine_test.go
+++ mock/mock_engine_test.go
@@ -282,8 +282,8 @@ func TestNewMockEngine_BuildResponse_SimpleValid_Pretty(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, 200, status)
-	assert.Equal(t, "{\n  \"category\": \"shirts\",\n  \"description\": \"A t-shirt with the pb33f logo on the"+
-		" front\",\n  \"id\": \"d1404c5c-69bd-4cd2-a4cf-b47c79a30112\",\n  \"image\": \"https://pb33f.io/images/t-shirt.png\",\n "+
+	assert.Equal(t, "{\n  \"category\": \"shirts\",\n  \"description\": \"A t-shirt with the pb33f logo "+
+		"on it.\",\n  \"id\": \"d1404c5c-69bd-4cd2-a4cf-b47c79a30112\",\n  \"image\": \"https://pb33f.io/images/t-shirt.png\",\n "+
 		" \"name\": \"pb33f t-shirt\",\n  \"price\": 19.99,\n  \"shortCode\": \"pb0001\"\n}", string(b))
 }
 
